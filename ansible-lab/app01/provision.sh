54231bcf703b406623cf0ddd28fceb672cb82d7a
Emersonft <emersonft@hotmail.com>
2023-02-09 00:13:56 -0300

    Configurar acesso do control node ao managed nodes

---
+++ ansible-lab/app01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDW+2POznBiB3Hfo2hxaL53WCzQcV8Egw9GFDHbwjjRXt55O5n5/CnxYx9yRS5nO504gDFeZFehcxLUyVehLBfL/vhp0oXv566iQ/koNhbgyrOVsfkqCHAk1Cs3h9Kj22f/SC6kNedFUBfLCCQ9yeH7CsEbSbQTdL4gZ2gPiuG6nbWIekawi6siTSxOGokHedlB88Z3Rqsan2a320Y9rulB145pvMqNT7ypNios+7ZxFe098BmpN1jXJBpPWSY5rfMMUSGA9QJ0cNEHtqyhhi/hWNgPmJcY0zdguV5G1g3gs7jLOOmgsryS5nbCWMSmgpoWeRVmCuvT6G6SajO/RvP vagrant@control-node
+EOT
